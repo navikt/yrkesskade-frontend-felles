0e05bcde54aa7492721e880ce0b4ffd2c85f1b99
Jone Lura <jone.lura@nav.no>
2022-02-03 08:24:10 +0100

    yrkesskade frontend felles

---
+++ .storybook/main.js
@@ -0,0 +1,30 @@
+module.exports = {
+    stories: ['../packages/**/*.stories.@(tsx|mdx)'],
+    addons: ['@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-storysource'],
+    typescript: {
+        check: true,
+        checkOptions: {},
+        reactDocgen: 'react-docgen-typescript',
+        reactDocgenTypescriptOptions: {
+            shouldExtractLiteralValuesFromEnum: true,
+        },
+    },
+    webpackFinal: async config => {
+        config.module.rules.push({
+            test: /\.(less)$/,
+            use: [
+                { loader: require.resolve('style-loader') },
+                { loader: require.resolve('css-loader'),
+                    options: {
+                        modules: {
+                            compileType: 'icss',
+                        },
+                    }
+                },
+                { loader: require.resolve('less-loader') },
+            ],
+        });
+        config.resolve.extensions.push('.ts', '.tsx', '.less');
+        return config;
+    },
+};
