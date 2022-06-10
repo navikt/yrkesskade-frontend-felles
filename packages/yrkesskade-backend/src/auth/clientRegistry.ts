76a18d05ad93b8ad8b4b6f28b7e3056a8f9c143f
Jone Lura <jone.lura@gmail.com>
2022-06-10 09:44:16 +0200

    YS
    - oppretter clientregistry for å samle openid klienter i en key-value state

--- packages/yrkesskade-backend/src/auth/clientRegistry.ts
+++ packages/yrkesskade-backend/src/auth/clientRegistry.ts
@@ -1,8 +1,12 @@
+import { logInfo } from '@navikt/yrkesskade-logging';
 import { BaseClient } from 'openid-client';
 
 const clientRegistry: { [key: string]: BaseClient } = {};
 
-const addClient = (key: string, client: BaseClient) => (clientRegistry[key] = client);
+const addClient = (key: string, client: BaseClient) => {
+    clientRegistry[key] = client;
+    logInfo(`${client.issuer} lagt til i clientRegistry med nøkkel ${key}`);
+};
 const getClient = (key: string) => clientRegistry[key];
 
 export default { addClient, getClient };
