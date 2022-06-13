3f600025f33559f058d345a6f09ee600c3ce7d55
Jone Lura <jone.lura@gmail.com>
2022-06-13 08:09:02 +0200

    YSMOD
    - la til IService til felles kode

--- packages/yrkesskade-backend/src/typer.ts
+++ packages/yrkesskade-backend/src/typer.ts
@@ -1,4 +1,19 @@
+/**
+ * Service kan benyttes til å definere proxy til eksterne
+ * integrasjoner.
+ */
+
 export interface IApi {
     clientId: string;
     scopes: string[];
+}
+
+export interface IService {
+    scope?: string;
+    audience?: string;
+    cluster: 'gcp' | 'fss';
+    displayName: string;
+    proxyPath: string;
+    id: string;
+    proxyUrl: string;
 }
